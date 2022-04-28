a8b934c200dba80953d01f632aa08e9a32cbc1ba
armaBurton <armanarma@armaburton.com>
2022-04-27 21:23:30 -0700

    I can now search my array, time to play some fallout

--- src/views/BurgerDetail/BurgerDetail.jsx
+++ src/views/BurgerDetail/BurgerDetail.jsx
@@ -10,26 +10,18 @@ export default function BurgerDetail({burgers}) {
     b.id === Number(id) ? profile = b : console.log(false);
   }
 
-// firstEpisode: "\"The Equestranauts\""
-// gender: "Male"
-// hairColor: "Brown"
-// id: 460
-// image: "https://bobsburgers-api.herokuapp.com/images/characters/460.jpg"
-// name: "Sunpuddle"
-// url: "https://bobsburgers-api.herokuapp.com/characters/460"
-// voicedBy: "Kurt Braunohler"
-// wikiUrl: "https://bobs-burgers.fandom.com/wiki/Sunpuddle"
-
   return(
     <Link to='/bobsBurgers'>
     <section className={style.burgerWrapper}>
       <div className={style.detailCard}>
-        <h1>{profile.name}</h1>
-        <div className={style.stats}>
-          <p><b><em>Gender:</em></b> {profile.gender}</p>
-          <p><b><em>Hair Color:</em></b> {profile.hairColor}</p>
-          <p><b><em>Voiced By:</em></b> {profile.voicedBy}</p>
-          <p><b><em>First Episode:</em></b> {profile.firstEpisode}</p>
+        <div className={style.info}>
+          <h1 className={style.name}>{profile.name}</h1>
+          <div className={style.stats}>
+            <p><b><em>Gender:</em></b> {profile.gender}</p>
+            <p><b><em>Hair Color:</em></b> {profile.hairColor}</p>
+            <p><b><em>Voiced By:</em></b> {profile.voicedBy}</p>
+            <p><b><em>First Episode:</em></b> {profile.firstEpisode}</p>
+          </div>
         </div>
         <img className={style.image} src={profile.image} alt={profile.name} />
       </div>
--- src/views/BurgerList/BurgerList.jsx
+++ src/views/BurgerList/BurgerList.jsx
@@ -1,19 +1,48 @@
 import BurgerRender from '../BurgerRender/BurgerRender'
 import burgerFetch from '../../services/burgerFetch';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import style from './BurgerList.css';
+import { Redirect } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
-export default function BurgerList({burgers}) {
+
+
+export default function BurgerList({burgers, bigArr}) {
+
+  const [searchArr, setSearchArr] = useState([bigArr]);
+  const [search, setSearch] = useState();
+  const [found, setFound] = useState();
+
+  console.log(bigArr);
+
+  function handleSubmit(){
+    console.log(typeof(search));
+    console.log(bigArr);
+    
+    const find = bigArr.filter(item => item.name.toLowerCase().includes(search));
+    
+    console.log(find);
+    
+    <Redirect to={`/bobBurgers/:${find.id}`}/>
+  }
+
+  function searchBurgers(e){
+    e.preventDefault();
+    handleSubmit();
+  }
 
   console.log(burgers);
   return(
     <>
       <div className={style.headSpace}>
       <p className={style.title}>BurgerList</p>
-      <form action="submit">
-        <input type="text" />
-        <button className={style.submitButton} type='submit'>search</button>
-      </form>
+      <div className={style.inputField}>
+        <input type="text" onChange={e => setSearch(e.target.value.toLowerCase())}/>
+        <button className={style.submitButton} onClick={searchBurgers}>search</button>
+      </div>
+      {/* <form onSubmit={searchBurgers}>
+        <input onChange={} />
+      </form> */}
       </div>
       <section className={style.dinnerTray}>
         <div className={style.burgerWrapper}>
